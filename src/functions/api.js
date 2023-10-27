0851c91ca931673cd33137e50f034630df86f99e
Joshua Eze <jezeadiebuo5@gmail.com>
2023-10-27 11:17:46 +0100

    another update

--- src/functions/api.js
+++ src/functions/api.js
@@ -26,7 +26,7 @@ const MySQLStore = require('express-mysql-session')(express_session_1.default);
 const path_1 = __importDefault(require("path"));
 const options = {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: +process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.NODE_ENV === "production" ? process.env.DB_PROD : process.env.DB_DEV,
--- src/functions/api.ts
+++ src/functions/api.ts
@@ -14,7 +14,7 @@ import path from "path";
 
 const options =  {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: +process.env.DB_PORT!,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.NODE_ENV === "production" ? process.env.DB_PROD : process.env.DB_DEV,
